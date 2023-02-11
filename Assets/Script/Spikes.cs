c71515a776c2c5bc43c1c296f0da6a6c625052ec
Léopold Lopez <leopold.lopez298@gmail.com>
2023-02-11 18:01:22 +0100

    Graphisme Piece, Sommet, Spike

--- Assets/Script/Spikes.cs
+++ Assets/Script/Spikes.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Spikes : MonoBehaviour {
 	private GameManager gameManager;
 
+	// Scripts
+	private Player_Movement player_mvt;
+
+    // GameObjects
+    public GameObject player;
+
 	private void Awake () {
+		player = GameObject.Find ("Player");
 		gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+		player_mvt = player.GetComponent<Player_Movement> ();
 	}
 
-	private void OnTriggerStay2D (Collider2D other) {
+	private void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag != "Player")
 			return;
 
-		if (other.transform.position.y >= transform.position.y) {
+		if (other.transform.position.y >= transform.position.y && !player_mvt.is7Calibur) {
 			gameManager.GameOver ();
 		}
 	}
